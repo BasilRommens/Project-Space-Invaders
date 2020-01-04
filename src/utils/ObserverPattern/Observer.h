7d5963533731f5bd2e541cbc5f2eee61c0281481
Basil Rommens <basil.rommens@student.uantwerpen.be>
2020-01-04 22:08:20 +0100

    updated documentation in observer
    Signed-off-by: Basil Rommens <basil.rommens@student.uantwerpen.be>

--- src/utils/ObserverPattern/Observer.h
+++ src/utils/ObserverPattern/Observer.h
@@ -12,12 +12,23 @@
 
 #include "../Event.h"
 
+/**
+ * @addtogroup Model
+ * @{
+ */
 namespace Model {
 
     class Entity;
 
 }
+/**
+ * @}
+ */
 
+/**
+ * @addtogroup ObserverPattern
+ * @{
+ */
 namespace ObserverPattern {
 
     class Observer {
@@ -33,9 +44,15 @@ namespace ObserverPattern {
          */
         virtual void onNotify(std::shared_ptr<Model::Entity> entity, Utils::Event event) = 0;
 
+        /**
+         * @return The type of the observer, when inherited it is especially useful
+         */
         virtual std::string getType();
     };
 
 }
+/**
+ * @}
+ */
 
 #endif //PROJECT_SPACE_INVADERS_OBSERVER_H
